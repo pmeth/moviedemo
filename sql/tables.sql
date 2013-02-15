d1d951d827cce9f30d7d7ab8d61c96deaefa4f5a
Peter Meth <pmeth@softersoftware.com>
2013-02-15 02:11:47 -0500

    made a decent list page

---
+++ sql/tables.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS `moviedemo`;
+
+USE `moviedemo`;
+
+DROP TABLE IF EXISTS `movies`;
+
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `year` int(4) DEFAULT NULL,
+  `thumb` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `date_added` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
